ec077a9679f01c00378503ff0bc18c345c8ca5d4
harmony.is.valued@gmail.com <53105202+zetzetn@users.noreply.github.com>
2020-05-17 18:55:06 +0900

    README更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_083037) do
+ActiveRecord::Schema.define(version: 2020_05_16_113541) do
 
   create_table "braves", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
