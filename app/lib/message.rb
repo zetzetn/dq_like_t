f6d81db3df8e03d817668bac650c90bb0bf15a5c
harmony.is.valued@gmail.com <53105202+zetzetn@users.noreply.github.com>
2020-06-07 16:58:03 +0900

    漢字をひらがなに変更

--- app/lib/message.rb
+++ app/lib/message.rb
@@ -1,29 +1,29 @@
 class Message
   class << self
     def level_up(brave)
-      "#{brave.name} のレベルが #{brave.level} に上がった！"
+      "#{brave.name} のレベルが #{brave.level} にあがった！"
     end
     
     def start(monster)
-      "#{monster.name} が現れた！"
+      "#{monster.name} があらわれた！"
     end
 
     def damage(attacker:, defender:)
       [
-        "#{attacker.name} の攻撃！",
+        "#{attacker.name} のこうげき！",
         "#{defender.name} に #{defender.damage} のダメージ！"
       ]
     end
 
     def victory(monster)
       [
-        "#{monster.name} を倒した！",
-        "#{monster.exp} の経験値を獲得！"
+        "#{monster.name} をたおした！",
+        "#{monster.exp} のけいけんちをかくとく！"
       ]
     end
 
     def defeat(brave)
-      "#{brave.name} は力尽きた……"
+      "#{brave.name} はちからつきた……"
     end
     
   end
